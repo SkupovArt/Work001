2c188cf92d9884fdc6c7dc648bf9d6fcd0cd6e57
SkupovArt <skripachmaster39@gmail.com>
2022-07-11 22:34:17 +0400

    Сделал все

--- number001/Program.cs
+++ number001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напиши программу, принимающую число и выдающую его квадрат
+
+
+Console.Write ("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double square = num * num;
+Console.Write ($"Квадрат числа {num} - {square}");
