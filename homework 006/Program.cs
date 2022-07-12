1d9c90b5592693c3b774d9ac8b6538454bbf30c8
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 20:32:25 +0400

    Homework 6 done

---
+++ homework 006/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число, а на выходе показывает послеюнюю цифрц этого числа
+// Типа 234 > 4
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+if (num > 999 )
+
+{
+    Console.WriteLine("ваше число не трехзначное");
+}
+else if (num < 100)
+{
+    Console.WriteLine("ваше число не трехзначное");
+}
+else
+{
+   res = num % 10;
+   Console.WriteLine($"Ваше число {res}, милорд");
+}
