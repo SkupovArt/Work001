f8a42578766e2d36d899f9a6b485ba51e72265ae
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 20:16:30 +0400

    homework001 done!

---
+++ Homework001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите свое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int antiNum = num * -1;
+int res = 0;
+while (num > antiNum)
+{
+    res = (int)num % 2;
+    if ((int)res == 0)
+    {
+        Console.Write($"{num}, ");
+    }
+    num = num - 1;
+}
