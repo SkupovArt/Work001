f091c60bfe01b50d839a34e54f039a5122ae509c
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 20:04:55 +0400

    homework 5 done

---
+++ Homework005/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите свое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int res = 0;
+while (num > 1)
+{
+    res = (int)num % 2;
+    if ((int)res == 0)
+    {
+        Console.Write($"{num}, ");
+    }
+    num = num - 1;
+}
