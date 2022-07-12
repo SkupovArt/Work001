4cc88c31058f07062c63bf81d30270fe8a46fe5c
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 19:49:23 +0400

    Homework 4 done

---
+++ Homework004/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдает, является ли оно четным (делится ли без остатка)
+Console.WriteLine("Введите свое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int res = (int)num % 2;
+if((int)res == 0)
+{
+    Console.WriteLine($"Число {num} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {num} не является четным");
+}
