0b4e9f2627f4cb6c3fc8679ca61980328e175c0f
SkupovArt <107272381+SkupovArt@users.noreply.github.com>
2022-07-13 11:26:22 +0400

    Delete number001 directory

--- number001/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Напиши программу, принимающую число и выдающую его квадрат
-
-
-Console.Write ("Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-double square = num * num;
-Console.Write ($"Квадрат числа {num} - {square}");
